ad365f319172c9abc9fbdb59de2e3233581d85eb
dlreddy <lingareddydaggula8@gmail.com>
2016-09-27 08:04:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,10 +5,40 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
-    title: 'Article One | dlreddy';
-    
+var articleOne = {
+    title: 'Article One | dlreddy',
+    heading: 'Article One',
+    date: '15 06 2016',
+    content: `<p>this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy.this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy..this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy.</p>
+            
+            <p>this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy.this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy..this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy.</p>
+            
+            <p>this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy.this is my first article so i have to do work hard to  get good result follow codecadamy adn khanacadamy..this is my first article so i have to do ff work hard to  get good result follow codecadamy adn khanacadamy.</p>
+        `,
 };
+
+
+
+var htmlTemplet =`<html>
+    <head>
+        <meta name="viewpoint" content='width-divice-width, initial-scale=1' />
+        <title>
+            ${title}
+        </title>
+    </head>
+    <body>
+        <div><a href="/">Home</a></div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>${date}</div>
+        <div>
+           ${content}
+        </div>
+    </body>
+</html>
+    
+    
+`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
