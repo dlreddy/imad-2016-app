976878118b51a3928ff88071edd751d98a59636f
dlreddy <lingareddydaggula8@gmail.com>
2016-09-29 15:33:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,13 @@
 console.log('Loaded!');
 
+//change the text of the main-text 
 var element = document.getElementById('main-text');
 
 element.innerHTML= 'new text display';
+
+//move image
+var img = document.getElementById('madi');
+img.onclick = function (){
+
+ img.style.marginRight = 50;
+};
